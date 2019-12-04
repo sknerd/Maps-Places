5105c933343d17c7c182e9bbdbd2ac013a87e568
Renald <renksss@gmail.com>
2019-12-04 14:01:14 +0300

    Initial Commit

---
+++ Maps&Places/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Maps&Places
+//
+//  Created by renks on 04.12.2019.
+//  Copyright © 2019 Renald Renks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
